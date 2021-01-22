a56136d7ae197a6cc4dac2b36d11dba4da199be2
sa-borgia3 <sa.borgia3@gmail.com>
2021-01-22 14:49:56 +0100

    home.blade di partenza

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <script src="{{ asset('js/app.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+
+    {{-- GOAL: sulla falsa riga di quanto visto questa mattina creare le seguenti rotte
+    SENZA controller:
+    - /helloAll: saluta tutti gli utenti in inglese (statico, no variabili)
+    - /helloYou: saluta utente per nome (variabile) in inglese
+    - /helloYou2: saluta utente per nome (stessa varbile, stessa view, ma nome diverso) in inglese
+    CON controller:
+    - /holaTodo: saluta tutti gli utenti in spagnolo
+    - /holaTu: saluta utente per nome (variabile) in spagnolo
+    - /holaTu2: saluta utente per nome (stessa varbile, stessa view, ma nome diverso) in spagnolo
+    NOTA: si ricorda di fare il tutto in NUOVO progetto, creato a partire dalla create-project (comando terminale)
+    --}}
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <h1></h1>
+        </div>
+    </div>
+</body>
+
+</html>
